88cba4f9299cd45778029de574b3bd5ed3db148c
SoniSarote <sonisarote1915@gmail.com>
2020-10-21 12:02:25 +0530

    constraints added

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint=Matter.Constraint;
+var constraintLog;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,7 +37,18 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log6=new Log(230,180,80,PI/2)
+chain=new Chain(bird.body,log6.body)
+    /*
+constraintLog=new Log(230,180,80,PI/2)
+var options={
+    bodyA:bird.body,
+    bodyB:constraintLog.body,
+    stiffness: 0.04,
+    length:10
+}
+var chain=Constraint.create(options)
+World.add(world,chain)*/
 }
 
 function draw(){
@@ -61,4 +74,8 @@ function draw(){
 
     bird.display();
     platform.display();
-}
+    log6.display();
+    chain.display();}
+    //constraintLog.display();
+   // strokeWeight(5)
+   // line (bird.body.position.x,bird.body.position.y,constraintLog.body.position.x,constraintLog.body.position.y)
